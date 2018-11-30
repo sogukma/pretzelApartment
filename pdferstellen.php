0d37dd2a5013fbb58dfb2193cd02124f5f3b9613
Necati <necati.van@bluewin.ch>
2018-11-30 21:11:00 +0100

    fpdf17, jquery eingefügt. pdferstellen.php erstellt manageInvoices.php geändert.

--- pdferstellen.php
+++ pdferstellen.php
@@ -15,39 +15,96 @@ $userId = $_GET['fk_user_id'];
 class myPDF extends FPDF{
     function header(){
         
-        $this->SetFont('Arial','B',14);
-        $this->Cell(276,5,'Pretzel Real Estate Inc.',0,0,'C');
-        $this->Ln();
-        $this->SetFont('Times','',12);
-        $this->Cell(276,10,'Rechnung',0,0,'C');
-        $this->Ln(20);
+        $this->SetFont('Arial', 'B', 14); //Font
+        $this->Image('pretzelIconsmall.png',10,5);
+        $this->Cell(189,30,'',0,1);//end of line
+        $this->Cell(130,5,'Pretzel Real Estate Inc.',0,0); //Head
+        $this->Cell(59,5,'',0,1);//end of line
+
+
+        $this->SetFont('Arial','', 12);
+        $this->Cell(30,2,'',0,1);
+        $this->Cell(130,5,'Musterstrasse 24',0,0); //Head
+        $this->Cell(59,5,'',0,1);//end of line
+
+        $this->Cell(130,5,'5000 Aarau',0,0); //Head
+        $this->Cell(25,5,'Datum',0,0);
+        $this->Cell(34,5,''.date("j.n.Y"),0,1);//end of line
+
+        $this->Cell(130,5,'Telefon: +41 62 876 90 43',0,0); //Head
+        $this->Cell(25,5,'Kunden-ID:',0,0);
+        $this->Cell(34,5,''.$_GET['fk_user_id'],0,1);//end of line
+
+        $this->Cell(130,5,'Fax: +41 62 876 90 40',0,0); //Head
+        $this->Cell(25,5,'',0,0);
+        $this->Cell(34,5,'',0,1);//end of line
+        
+        $this->Cell(130,5,'info@pretzelrealestate.ch',0,0); //Head
+        $this->Cell(25,5,'',0,0);
+        $this->Cell(34,5,'',0,1);//end of line
+
+        //dummy empty cell, vertical
+        $this->Cell(189,15,'',0,1);//end of line
+
+        //billing adress
+        
+        $this->Cell(100,5,'Rechnung an: ',0,1);//end of line
+
+        //ad dummy cell at beginning of each line
+        $this->Cell(30,5,'',0,0);
+        $this->Cell(70,5,'Vorname Name',0,1); //aus DB holen
+        
+        $this->Cell(30,5,'',0,0);
+        $this->Cell(70,5,'Strasse',0,1); //aus DB holen
+        
+
+        //dummy empty cell, vertical
+        $this->Cell(189,15,'',0,1);//end of line
     }
+    
+    function footer(){
+        $this->SetY(-15);
+        $this->SetFont('Arial','',8);
+        $this->Cell(0,10,'Seite '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+    
     function headerTable(){
-        $this->SetFont('Times','B',12);
-        $this->Cell(20,10,'ID',1,0,'C');
-        $this->Cell(40,10,'Rechnungtyp',1,0,'C');
-        $this->Cell(40,10,'Status',1,0,'C');
-        $this->Cell(40,10,'Betrag',1,0,'C');
-        $this->Cell(36,10,'User-ID',1,0,'C');
-        $this->Ln();
-    }
-    function viewTable($db, $userId){
-        $this->SetFont('Times','',12);
-        $stmt = $db->query("select * from rechnung WHERE fk_userId = '$userId'");
-        while($data = $stmt->fetch(PDO::FETCH_OBJ)){
-            $this->Cell(20,10,$data->id,1,0,'L');
-            $this->Cell(40,10,$data->rechnungstyp,1,0,'L');
-            $this->Cell(40,10,$data->status,1,0,'L');
-            $this->Cell(40,10,$data->betrag,1,0,'L');
-            $this->Cell(36,10,$data->fk_userId,1,0,'L');
-            $this->Ln();
-        }
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(25,7,'ID',1,0);
+        $this->Cell(65,7,'Rechnungstyp',1,0);
+        $this->Cell(35,7,'Betrag',1,1); //end of line
         
     }
+    
+    function viewTable($db, $userId){
+        $this->SetFont('Arial','',12);
+        $stmt = $db->query("select * from rechnung WHERE fk_userId = '$userId' AND status = 'offen'");
+        while($data = $stmt->fetch(PDO::FETCH_OBJ)){
+                                  
+            $this->SetFont('Arial', '', 12);
+            $this->Cell(25,7,$data->id,1,0);
+            $this->Cell(65,7,$data->rechnungstyp,1,0);
+            $this->Cell(35,7,$data->betrag,1,1,'R'); 
+        }
+        //$sum= mysql_query("select sum(betrag) from rechnung WHERE status = 'offen'");
+        //$row = mysql_fetch_array($sum);
+        
+        $sum = $db->query("select sum(betrag) from rechnung WHERE status = 'offen'");
+        $data2 = $sum->fetch(PDO::FETCH_OBJ);
+        
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(25,7,'',0,0);
+        $this->Cell(65,7,'                                       Summe',0,0);
+        $this->Cell(11,7,CHF,0,0);
+        $this->Cell(24,7,$data2,1,1); //end of line
+   }
 }
+
+
+
 $pdf = new myPDF();
 $pdf->AliasNbPages();
-$pdf->AddPage('L','A4',0);
+$pdf->AddPage('P','A4',0);
 $pdf->headerTable();
 $pdf->viewTable($db, $userId);
 $pdf->Output();
