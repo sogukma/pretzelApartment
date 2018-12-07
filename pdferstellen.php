31b638531cd261081cd96505f53e0db644e0ee24
Necati <necati.van@bluewin.ch>
2018-12-07 22:22:32 +0100

    fpdf17, jquery eingefügt. pdferstellen.php erstellt manageInvoices.php geändert.

--- pdferstellen.php
+++ pdferstellen.php
@@ -5,7 +5,7 @@
  * Die Rechnungen werden übersichtlich in einem PDF-Format generiert.
  *
  * @category   View, Model
- * @author     Necati (code), (design)
+ * @author     Necati (code, design)
  */
 
  include 'sessionHandling.php';
@@ -22,18 +22,20 @@ $userId = $_GET['fk_user_id'];
 
 
 class myPDF extends FPDF{
-    function header(){
+    function header1($db, $userId){
         //Company Adress
         $this->SetFont('Arial', 'B', 12); //Font
         $this->Image('pretzelIconsmall.png',10,5);
         $this->Cell(189,30,'',0,1);//end of line
         $this->Cell(130,5,'Pretzel Real Estate Inc.',0,0);
-        $this->Cell(59,5,'',0,1);//end of line
+        $this->Cell(59,5,'',0,1);
 
-
+        $stmt7 = $db->query("select user_streetnumber from users WHERE user_id = '$userId'");
+        $data7 = $stmt7->fetch(PDO::FETCH_OBJ);
+        
         $this->SetFont('Arial','', 12);
         $this->Cell(30,2,'',0,1);
-        $this->Cell(130,5,'Musterstrasse 24',0,0);
+        $this->Cell(130,5,'Musterstrasse '.$data7->user_streetnumber,0,0);
         $this->Cell(59,5,'',0,1);
 
         $this->Cell(130,5,'4001 Basel',0,0);
@@ -46,21 +48,21 @@ class myPDF extends FPDF{
 
         $this->Cell(130,5,'Fax: +41 62 876 90 40',0,0);
         $this->Cell(25,5,'',0,0);
-        $this->Cell(34,5,'',0,1);//end of line
+        $this->Cell(34,5,'',0,1);
         
         $this->Cell(130,5,'info@pretzelrealestate.ch',0,0);
         $this->Cell(25,5,'',0,0);
-        $this->Cell(34,5,'',0,1);//end of line
+        $this->Cell(34,5,'',0,1);
 
-        //dummy empty cell, vertical
+        //empty cell, vertical
         $this->Cell(189,15,'',0,1);
         
-        //billing adress
+        //"Rechnung"-Titel
         $this->SetFont('Arial', 'B', 15); //Font
         $this->Cell(130,5,'Rechnung',0,0);
         $this->Cell(25,5,'',0,0);
         $this->Cell(34,5,'',0,1);
-        $this->Cell(189,3,'',0,1);
+        $this->Cell(189,5,'',0,1); //empty cell vertical
 
         $this->SetFont('Arial', '', 12); //Font   
    }
@@ -71,28 +73,23 @@ class myPDF extends FPDF{
         
         $stmt3 = $db->query("select user_surname from users WHERE user_id = '$userId'");
         $data3 = $stmt3->fetch(PDO::FETCH_OBJ);
-        
-        $stmt4 = $db->query("select user_streetnumber from users WHERE user_id = '$userId'");
-        $data4 = $stmt4->fetch(PDO::FETCH_OBJ);
-        
+               
         $this->Cell(30,5,'An: ',0,0);
         $this->Cell(15,5,$data2->user_firstname.' '.$data3->user_surname,0,1);
         $this->Cell(30,5,'',0,0);
-        $this->Cell(70,5,$data4->user_streetnumber,0,1);
-        $this->Cell(189,15,'',0,1); //empty cell
+        $this->Cell(189,13,'',0,1); //empty cell
    } 
     
- 
-    function headerTable(){
+    //head (titel) of the table
+    function tableHead(){
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(25,7,'ID',1,0);
         $this->Cell(50,7,'Rechnungstyp',1,0);
         $this->Cell(35,7,'Gestellt am',1,0);
         $this->Cell(35,7,'Betrag',1,1); //end of line
-        
     }
-    
-    function viewTable($db, $userId){
+    //Body of the table
+    function tableBody($db, $userId){
         $this->SetFont('Arial','',12);
         $stmt = $db->query("select * from rechnung WHERE fk_userId = '$userId' AND status = 'offen'");
         while($data = $stmt->fetch(PDO::FETCH_OBJ)){
@@ -105,17 +102,17 @@ class myPDF extends FPDF{
         }
         
     }
-    
+    //total cost of bills
     function sum($db, $userId){
         
-        $stmt4 = $db->query("select sum(betrag) from rechnung WHERE fk_userId = '$userId' AND status = 'offen'");
+        $stmt4 = $db->query("select sum(betrag) as sumbetrag from rechnung WHERE fk_userId = '$userId' AND status = 'offen'");
         $data4 = $stmt4->fetch(PDO::FETCH_OBJ);
         
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(25,7,'',0,0);
         $this->Cell(74,7,'                                           Summe',0,0);
         $this->Cell(11,7,'CHF',0,0);
-        //$this->Cell(35,7,$data4->sum(betrag),1,1,'R');
+        $this->Cell(35,7,$data4->sumbetrag,1,1,'R');
         
     }
 
@@ -133,9 +130,10 @@ class myPDF extends FPDF{
 $pdf = new myPDF();
 $pdf->AliasNbPages();
 $pdf->AddPage('P','A4',0);
+$pdf->header1($db, $userId);
 $pdf->useradress($db, $userId);
-$pdf->headerTable();
-$pdf->viewTable($db, $userId);
+$pdf->tableHead();
+$pdf->tableBody($db, $userId);
 $pdf->sum($db, $userId);
 $pdf->Output();
 
