9e645f4c75b609bb492cd64e9ec4a4d0cab82239
Necati <necati.van@bluewin.ch>
2018-12-07 15:33:42 +0100

    fpdf17, jquery eingefügt. pdferstellen.php erstellt manageInvoices.php geändert.

--- pdferstellen.php
+++ pdferstellen.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * pdferstellen.php
+ *
+ *Hier werden die offenen Rechnungen als ein PDF-Datei generiert.
+ *
+     
+ */
  include 'sessionHandling.php';
     $sh = sessionHandling::Instance();
         $sh->open_session(); //vorhandene session übernehmen
@@ -14,64 +21,79 @@ $userId = $_GET['fk_user_id'];
 
 class myPDF extends FPDF{
     function header(){
-        
-        $this->SetFont('Arial', 'B', 14); //Font
+        //Company Adress
+        $this->SetFont('Arial', 'B', 12); //Font
         $this->Image('pretzelIconsmall.png',10,5);
         $this->Cell(189,30,'',0,1);//end of line
-        $this->Cell(130,5,'Pretzel Real Estate Inc. TETS',0,0); //Head
+        $this->Cell(130,5,'Pretzel Real Estate Inc.',0,0);
         $this->Cell(59,5,'',0,1);//end of line
 
 
         $this->SetFont('Arial','', 12);
         $this->Cell(30,2,'',0,1);
-        $this->Cell(130,5,'Musterstrasse 24',0,0); //Head
-        $this->Cell(59,5,'',0,1);//end of line
+        $this->Cell(130,5,'Musterstrasse 24',0,0);
+        $this->Cell(59,5,'',0,1);
 
-        $this->Cell(130,5,'5000 Aarau',0,0); //Head
-        $this->Cell(25,5,'Datum',0,0);
-        $this->Cell(34,5,''.date("j.n.Y"),0,1);//end of line
+        $this->Cell(130,5,'4001 Basel',0,0);
+        $this->Cell(25,5,'Datum:',0,0);
+        $this->Cell(34,5,''.date("j.n.Y"),0,1);
 
-        $this->Cell(130,5,'Telefon: +41 62 876 90 43',0,0); //Head
+        $this->Cell(130,5,'Telefon: +41 62 876 90 43',0,0);
         $this->Cell(25,5,'Kunden-ID:',0,0);
-        $this->Cell(34,5,''.$_GET['fk_user_id'],0,1);//end of line
+        $this->Cell(34,5,''.$_GET['fk_user_id'],0,1);
 
-        $this->Cell(130,5,'Fax: +41 62 876 90 40',0,0); //Head
+        $this->Cell(130,5,'Fax: +41 62 876 90 40',0,0);
         $this->Cell(25,5,'',0,0);
         $this->Cell(34,5,'',0,1);//end of line
         
-        $this->Cell(130,5,'info@pretzelrealestate.ch',0,0); //Head
+        $this->Cell(130,5,'info@pretzelrealestate.ch',0,0);
         $this->Cell(25,5,'',0,0);
         $this->Cell(34,5,'',0,1);//end of line
 
         //dummy empty cell, vertical
-        $this->Cell(189,15,'',0,1);//end of line
-
+        $this->Cell(189,15,'',0,1);
+        
         //billing adress
-        
-        $this->Cell(100,5,'Rechnung an: ',0,1);//end of line
+        $this->SetFont('Arial', 'B', 14); //Font
+        $this->Cell(130,5,'Rechnung',0,0);
+        $this->Cell(25,5,'',0,0);
+        $this->Cell(34,5,'',0,1);
+        $this->Cell(189,3,'',0,1);
 
-        //ad dummy cell at beginning of each line
-        $this->Cell(30,5,'',0,0);
-        $this->Cell(70,5,'Vorname Name',0,1); //aus DB holen
+        $this->SetFont('Arial', '', 12); //Font
+        
+        //Individual adress (doesnt work)
+        function viewTable2($db, $userId){
+        $stmt2 = $db->query("select user_streetnumber from users WHERE user_id = '$userId'");
+        $data = $stmt2->fetch(PDO::FETCH_OBJ);
+     
+        //$this->Cell(30,5,'',0,0);
+        //$this->Cell(70,5,$data->user_streetnumber,1,1);
+   }
+                
+        $this->Cell(30,5,'An:',0,0);
+        $this->Cell(70,5,'Vorname Name',0,1); //<----------------------------
         
         $this->Cell(30,5,'',0,0);
-        $this->Cell(70,5,'Strasse',0,1); //aus DB holen
+        $this->Cell(70,5,'Strasse',0,1); //<----------------------------------
         
 
         //dummy empty cell, vertical
         $this->Cell(189,15,'',0,1);//end of line
     }
     
+    //page number
     function footer(){
         $this->SetY(-15);
-        $this->SetFont('Arial','',8);
+        $this->SetFont('Arial','',10);
         $this->Cell(0,10,'Seite '.$this->PageNo().'/{nb}',0,0,'C');
     }
     
     function headerTable(){
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(25,7,'ID',1,0);
-        $this->Cell(65,7,'Rechnungstyp',1,0);
+        $this->Cell(50,7,'Rechnungstyp',1,0);
+        $this->Cell(35,7,'Gestellt am',1,0);
         $this->Cell(35,7,'Betrag',1,1); //end of line
         
     }
@@ -83,30 +105,40 @@ class myPDF extends FPDF{
                                   
             $this->SetFont('Arial', '', 12);
             $this->Cell(25,7,$data->id,1,0);
-            $this->Cell(65,7,$data->rechnungstyp,1,0);
+            $this->Cell(50,7,$data->rechnungstyp,1,0);
+            $this->Cell(35,7,$data->gestellt_am,1,0);
             $this->Cell(35,7,$data->betrag,1,1,'R'); 
         }
+        //Betrag sum (doenst work)
+        
         //$sum= mysql_query("select sum(betrag) from rechnung WHERE status = 'offen'");
         //$row = mysql_fetch_array($sum);
-        
+                        
         //$sum = $db->query("select sum(betrag) from rechnung WHERE status = 'offen'");
         //$data2 = $sum->fetch(PDO::FETCH_OBJ);
         
+        //$query = "select sum(betrag) from rechnung WHERE status = 'offen'";  
+        //$result = mysql_query($query); 
+        //$row = mysql_fetch_array($result);
+        
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(25,7,'',0,0);
-        $this->Cell(65,7,'                                       Summe',0,0);
+        $this->Cell(74,7,'                                           Summe',0,0);
         $this->Cell(11,7,'CHF',0,0);
-        $this->Cell(24,7,'',1,1); //end of line
+        $this->Cell(35,7,'PREIS',1,1,'R'); //<---------------------------
    }
+   
+   
 }
 
-
+//generating PDF
 
 $pdf = new myPDF();
 $pdf->AliasNbPages();
 $pdf->AddPage('P','A4',0);
 $pdf->headerTable();
 $pdf->viewTable($db, $userId);
+//$pdf->viewTable2($db, $userId);
 $pdf->Output();
 
 ?>
