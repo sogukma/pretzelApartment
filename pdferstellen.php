61efc3994ce025a566f36925a78711cffa9b335a
Necati <necati.van@bluewin.ch>
2018-12-09 15:11:55 +0100

    pdferstellen.pdf fertig

--- pdferstellen.php
+++ pdferstellen.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * pdferstellen.php
  *
@@ -9,46 +8,46 @@
  * @category   View, Model
  * @author     Necati (code, design)
  */
+
 include 'sessionHandling.php';
 $sh = sessionHandling::Instance();
 $sh->open_session(); //vorhandene session übernehmen
 $sh->regenerate_session_id();
 $sh->isCorrectPape("abwart");
 
-
 require("fpdf.php");
 $db = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_264d8c0ac2b1d8d', 'b46bd03f9f4fdf', 'cb61f804');
+
 $userId = $_GET['fk_user_id'];
+define('u',chr(252));
 
 class myPDF extends FPDF {
 
-    function header1($db, $userId) {
+    //picture
+    function pix(){
+        $this->Image('pretzelIconsmall.png', 166, 8);
+        $this->Cell(189, 3, '', 0, 1); //end of line
+    }
+    
+    //company adress
+    function company($db, $userId) {
         //Company Adress
         $this->SetFont('Arial', 'B', 12); //Font
-        $this->Image('pretzelIconsmall.png', 10, 5);
-        $this->Cell(189, 30, '', 0, 1); //end of line
         $this->Cell(130, 5, 'Pretzel Real Estate Inc.', 0, 0);
         $this->Cell(59, 5, '', 0, 1);
-
-
-        $stmt7 = $db->prepare("select user_streetnumber from users WHERE user_id = ?");
-        $stmt7->bindParam(1, $userId);
-        $stmt7->execute();
-
-        $data7 = $stmt7->fetch(PDO::FETCH_OBJ);
-
+        
         $this->SetFont('Arial', '', 12);
         $this->Cell(30, 2, '', 0, 1);
-        $this->Cell(130, 5, 'Bergstrasse ' . $data7->user_streetnumber, 0, 0);
+        $this->Cell(130, 5, 'Herbrigstrasse 29', 0, 0);
         $this->Cell(59, 5, '', 0, 1);
 
-        $this->Cell(130, 5, '4001 Basel', 0, 0);
-        $this->Cell(25, 5, 'Datum:', 0, 0);
-        $this->Cell(34, 5, '' . date("j.n.Y"), 0, 1);
+        $this->Cell(130, 5, '9011 St. Gallen', 0, 0);
+        $this->Cell(25, 5, '', 0, 0);
+        $this->Cell(34, 5, '', 0, 1);
 
         $this->Cell(130, 5, 'Telefon: +41 62 876 90 43', 0, 0);
-        $this->Cell(25, 5, 'Kunden-ID:', 0, 0);
-        $this->Cell(34, 5, '' . $_GET['fk_user_id'], 0, 1);
+        $this->Cell(25, 5, '', 0, 0);
+        $this->Cell(34, 5, '', 0, 1);
 
         $this->Cell(130, 5, 'Fax: +41 62 876 90 40', 0, 0);
         $this->Cell(25, 5, '', 0, 0);
@@ -58,20 +57,12 @@ class myPDF extends FPDF {
         $this->Cell(25, 5, '', 0, 0);
         $this->Cell(34, 5, '', 0, 1);
 
-        //empty cell, vertical
-        $this->Cell(189, 15, '', 0, 1);
-
-        //"Rechnung"-Titel
-        $this->SetFont('Arial', 'B', 15); //Font
-        $this->Cell(130, 5, 'Rechnung', 0, 0);
-        $this->Cell(25, 5, '', 0, 0);
-        $this->Cell(34, 5, '', 0, 1);
-        $this->Cell(189, 5, '', 0, 1); //empty cell vertical
-
-        $this->SetFont('Arial', '', 12); //Font   
+        $this->Cell(189, 15, '', 0, 1); //empty cell, vertical
+           
     }
-
+    // renter adress
     function useradress($db, $userId) {
+        $this->SetFont('Arial', '', 12); //Font
         $stmt2 = $db->prepare("select user_firstname from users WHERE user_id = ?");
         $stmt2->bindParam(1, $userId);
         $stmt2->execute();
@@ -80,13 +71,37 @@ class myPDF extends FPDF {
         $stmt3 = $db->prepare("select user_surname from users WHERE user_id = ?");
         $stmt3->bindParam(1, $userId);
         $stmt3->execute();
-
         $data3 = $stmt3->fetch(PDO::FETCH_OBJ);
-
-        $this->Cell(30, 5, 'An: ', 0, 0);
-        $this->Cell(15, 5, $data2->user_firstname . ' ' . $data3->user_surname, 0, 1);
-        $this->Cell(30, 5, '', 0, 0);
+        
+        $stmt7 = $db->prepare("select user_streetnumber from users WHERE user_id = ?");
+        $stmt7->bindParam(1, $userId);
+        $stmt7->execute();
+        $data7 = $stmt7->fetch(PDO::FETCH_OBJ);
+        
+        $this->Cell(30, 5, $data2->user_firstname . ' ' . $data3->user_surname, 0, 1);
+        $this->Cell(30, 5, 'Bergstrasse ' . $data7->user_streetnumber, 0, 1);
+        $this->Cell(30, 5, '4051 Basel', 0, 1);
         $this->Cell(189, 13, '', 0, 1); //empty cell
+    }
+    
+     //"Rechnung"-title
+    function adress($db, $userId){
+        $stmt7 = $db->prepare("select user_streetnumber from users WHERE user_id = ?");
+        $stmt7->bindParam(1, $userId);
+        $stmt7->execute();
+        $data7 = $stmt7->fetch(PDO::FETCH_OBJ);
+        
+        $this->SetFont('Arial', '', 12);
+        $this->Cell(25, 5, 'St. Gallen, '. date("j.n.Y"), 0, 1);
+        $this->Cell(189, 5, '', 0, 1);
+        $this->SetFont('Arial', 'B', 15); //Font
+        $this->Cell(130, 5, 'Offene Rechnungen', 0, 1);
+        $this->SetFont('Arial', '', 12);
+        $this->Cell(189, 5, '', 0, 1);
+        $this->Cell(189, 5, 'Sehr geehrte Damen und Herren', 0, 1);
+        $this->Cell(189, 5, 'F'.u.'r Ihre Wohnung an der Bergstrasse '.$data7->user_streetnumber.' erhalten Sie die zu begleichenden Rechnungen.', 0, 1);
+        $this->Cell(189, 5, 'Diese sind zahlbar innert zwei Arbeitswochen.', 0, 1);
+        $this->Cell(189, 8, '', 0, 1); //empty cell vertical
     }
 
     //head (titel) of the table
@@ -129,23 +144,31 @@ class myPDF extends FPDF {
         $this->Cell(11, 7, 'CHF', 1, 0);
         $this->Cell(24, 7, $data4->sumbetrag, 1, 1, 'R');
     }
-
-    //page number
+    
+    //page number and greetings
     function footer() {
-        $this->SetY(-15);
         $this->SetFont('Arial', '', 12);
+        $this->Cell(25, 10, '', 0, 1);
+        $this->Cell(11, 5, 'Bei Fragen und Unklarheiten stehen wir Ihnen gerne zu Verf'.u.'gung.', 0, 1);
+        $this->Cell(11, 13, '', 0, 1);
+        $this->Cell(11, 5, 'Mit freundlichen Gr'.u.'ssen', 0, 1);
+        $this->Cell(11, 5, '', 0, 1);
+        $this->Cell(11, 5, 'Alain Ernst', 0, 1);
+        $this->Cell(11, 5, 'Verwalter Mietobjekte, Region Basel', 0, 1);
+        $this->SetY(-15);
+        $this->SetFont('Arial', '', 10);
         $this->Cell(0, 10, 'Seite ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
     }
-
 }
 
 //generating PDF
-
 $pdf = new myPDF();
 $pdf->AliasNbPages();
 $pdf->AddPage('P', 'A4', 0);
-$pdf->header1($db, $userId);
+$pdf->pix();
+$pdf->company($db, $userId);
 $pdf->useradress($db, $userId);
+$pdf->adress($db, $userId);
 $pdf->tableHead();
 $pdf->tableBody($db, $userId);
 $pdf->sum($db, $userId);
